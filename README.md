fb8297848f4c2745f380db29599aaa821d06c364
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-14 14:46:07 -0600

    Initial push to repo plus random image generator function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# HTML Boilerplate
+# Giphy API
--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+const img = document.querySelector('img');
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=9b1DIYcUHy5v1sIRqdKrVCzevE0anL6J&s=cats', {mode: 'cors'})
+  .then((res) => {
+    return res.json();
+  })
+  .then((res)=>{
+    //console.log(res.data.images.original.url);
+    img.src = res.data.images.original.url;
+  });
